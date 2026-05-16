eabfa7fb7869219ca865a51a4e98cc868280efea
Concelare <113200622+Concelare@users.noreply.github.com>
2026-05-16 17:16:35 +0100

    Add system message for unverified players in CommandEvent handling

--- src/events/command.rs
+++ src/events/command.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use pumpkin_plugin_api::events::{EventHandler, FromIntoEvent, PlayerCommandSendEvent};
 use pumpkin_plugin_api::Server;
+use pumpkin_plugin_api::text::TextComponent;
 use uuid::Uuid;
 use crate::services::auth::{AUTH_SERVICE, UNVERIFIED};
 
@@ -9,26 +10,26 @@ pub struct CommandEvent;
 impl EventHandler<PlayerCommandSendEvent> for CommandEvent {
     fn handle(&self, server: Server, mut event: <PlayerCommandSendEvent as FromIntoEvent>::Data) -> <PlayerCommandSendEvent as FromIntoEvent>::Data {
         let player = &event.player;
-        
+
         let unverified = match UNVERIFIED.get() {
             Some(unverified) => unverified,
             None => return event,
         };
-        
+
         let lock = match unverified.lock() {
             Ok(lock) => lock,
             Err(_) => return event,
         };
-        
+
         let uuid = match Uuid::from_str(player.get_id().as_str()) {
             Ok(uuid) => uuid,
             Err(_) => return event,
         };
-        
+
         if lock.contains(&uuid) {
+            event.player.send_system_message(TextComponent::text("You are not verified! You cannot use commands. Use /staff login!"), true);
             event.cancelled = true;
         }
-        
         event
     }
 }
